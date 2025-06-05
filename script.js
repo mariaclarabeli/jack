4299e9839345495b28f70b06810d876f886b125f
mariaclarabeli <164515325+mariaclarabeli@users.noreply.github.com>
2025-06-05 10:23:27 -0300

    adwas

--- script.js
+++ script.js
@@ -28,4 +28,4 @@ document.addEventListener('DOMContentLoaded', function () {
     alternaContraste.addEventListener('click', function () {
         document.body.classList.toggle('alto-contraste')
     })
-})      
+})
